5302893c56c4bbf9b37824c2b4333669a089fe72
JL Falcone <jean-luc.falcone@unige.ch>
2009-09-05 14:17:22 +0200

    Added class output for graphml

--- src/cxa/extra/export/GraphMLFormat.java
+++ src/cxa/extra/export/GraphMLFormat.java
@@ -24,8 +24,18 @@ public class GraphMLFormat implements ExportFormat {
     public void export( CxA cxa, PrintStream ps ) throws IOException {
 
         Element graphml = new Element( "graphml", NS );
-        graphml.addNamespaceDeclaration( "xsi", "http://www.w3.org/2001/XMLSchema-instance");
-        graphml.addAttribute( new Attribute( "xsi:schemaLocation", "http://www.w3.org/2001/XMLSchema-instance","http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd" ) );
+        graphml.addNamespaceDeclaration( "xsi", "http://www.w3.org/2001/XMLSchema-instance" );
+        graphml.addAttribute(
+                new Attribute(
+                "xsi:schemaLocation",
+                "http://www.w3.org/2001/XMLSchema-instance",
+                "http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd" ) );
+        Element classKey = new Element( "key", NS );
+        classKey.addAttribute( new Attribute( "id", "class" ) );
+        classKey.addAttribute( new Attribute( "for", "all" ) );
+        classKey.addAttribute( new Attribute( "attr.name", "class" ) );
+        classKey.addAttribute( new Attribute( "attr.type", "string" ) );
+        graphml.appendChild( classKey );
         Element graph = new Element( "graph", NS );
         graph.addAttribute( new Attribute( "id", cxa.ID() ) );
         graph.addAttribute( new Attribute( "edgedefault", "directed" ) );
@@ -33,6 +43,7 @@ public class GraphMLFormat implements ExportFormat {
         for( Kernel k : cxa.getKernels() ) {
             Element node = new Element( "node", NS );
             node.addAttribute( new Attribute( "id", k.ID() ) );
+            fillClassKey( k ,node);
             graph.appendChild( node );
         }
         for( Conduit c : cxa.getConduits() ) {
@@ -40,13 +51,21 @@ public class GraphMLFormat implements ExportFormat {
             edge.addAttribute( new Attribute( "id", c.ID() ) );
             edge.addAttribute( new Attribute( "source", c.getSenderKernel().ID() ) );
             edge.addAttribute( new Attribute( "target", c.getReceiverKernel().ID() ) );
+            fillClassKey( c, edge );
             graph.appendChild( edge );
         }
         Document doc = new Document( graphml );
-        Serializer serializer = new Serializer( new BufferedOutputStream( ps), "UTF8" );
+        Serializer serializer = new Serializer( new BufferedOutputStream( ps ), "UTF8" );
         serializer.setIndent( 2 );
         serializer.setMaxLength( 80 );
         serializer.write( doc );
 
+    }
+
+    private void fillClassKey( Object o, Element node ) {
+        Element data = new Element( "data", NS );
+        data.addAttribute( new Attribute( "key", "class" ) );
+        data.appendChild( o.getClass().getName() );
+        node.appendChild( data );
     }
 }
