3ffda9b6bb6302cabfe1f0f750c7c1df5be8d6ba
JL Falcone <jean-luc.falcone@unige.ch>
2009-09-06 17:15:43 +0200

    Added multiscale interface

---
+++ src/cxa/extra/multiscale/Scaled.java
@@ -0,0 +1,10 @@
+package cxa.extra.multiscale;
+
+/**
+ * Marks an object with space and time scale
+ */
+public interface Scaled {
+
+    public Scale getScale();
+
+}
---
+++ src/cxa/extra/multiscale/components/ScaledKernel.java
@@ -0,0 +1,10 @@
+package cxa.extra.multiscale.components;
+
+import cxa.extra.multiscale.*;
+import cxa.components.kernel.Kernel;
+
+/**
+ *
+ * @author falcone
+ */
+public interface ScaledKernel extends Kernel, Scaled { }
